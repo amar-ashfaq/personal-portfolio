67ae38e8d1eb2115b3fdcd449a1230512e05aa3d
Amar AA <amar.ashfaq95@gmail.com>
2025-07-08 16:53:57 +0100

    Next.js, Tailwind, TypeScript project setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <h1 className="text-white-600 text-5xl font-bold">
+        Tailwind is working! 🎉
+      </h1>
+    </main>
+  );
+}
